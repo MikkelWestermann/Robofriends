6ee17d5d8ccf03353fc2aa06644fb17cbafb435c
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-11-07 19:13:16 +0100

    Structure files + clean up

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import { robots } from './robots';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll'; 
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import Scroll from '../Components/Scroll';
 import './App.css';
 
 class App extends Component {
@@ -17,7 +16,7 @@ class App extends Component {
 componentDidMount () {
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(users => this.setState({ robots: robots }))
+  .then(users => this.setState({ robots: users }))
 }
 
   onSearchChange = (event) => {
@@ -25,13 +24,13 @@ componentDidMount () {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else {
-      return (
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
         <div className="tc">
           <h1 className="f1">Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
@@ -40,7 +39,6 @@ componentDidMount () {
           </Scroll>
         </div>
       );
-    }
   }
 }
 
