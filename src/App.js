81d99baf987156fd05fa27581b939afe93ce9cf2
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-11-07 15:22:16 +0100

    Add fetching of users from web

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
+
+componentDidMount () {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ robots: robots }))
+}
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
@@ -20,14 +26,18 @@ class App extends Component {
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    });
-    return (
-      <div className="tc">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    })
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={filteredRobots}/>
+        </div>
+      );
+    }
   }
 }
 
