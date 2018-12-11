6faf12ed1af6aee2ef5e7d5587e56256eb9f43ef
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-12-11 11:57:50 +0100

    Updates

---
+++ precache-manifest.77b4edf8afc6b5ef9aa6eba06844b2af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f28426ed58002a6d1522",
+    "url": "/Robofriends/static/css/main.5d1f1b7b.chunk.css"
+  },
+  {
+    "revision": "f28426ed58002a6d1522",
+    "url": "/Robofriends/static/js/main.f28426ed.chunk.js"
+  },
+  {
+    "revision": "737a5eb5a128bf0c74fe",
+    "url": "/Robofriends/static/css/1.33fda255.chunk.css"
+  },
+  {
+    "revision": "737a5eb5a128bf0c74fe",
+    "url": "/Robofriends/static/js/1.737a5eb5.chunk.js"
+  },
+  {
+    "revision": "39a3a35c71e0df973673",
+    "url": "/Robofriends/static/js/runtime~main.39a3a35c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "613ffcc4f749bdf26a72f901abff82fb",
+    "url": "/Robofriends/index.html"
+  }
+];
